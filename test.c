c77a52c27ea722c5155aa9439010596e58276506
Tripp R <60992655+RealTrippR@users.noreply.github.com>
2025-08-25 00:07:51 -0400

    init

--- test.c
+++ test.c
@@ -2,26 +2,32 @@
 #include <crtdbg.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+
+#define THREAD_TIMEOUT_MS 60000 // 60 seconds
 void* printHello(void*__args__)
 {
-    printf("Hello");
+    printf("Hello\n");
 }
 int main(int argc, char **argv)
 {
     _CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF);
     ThreadPoolHandle tpHdl;
     tpHdl.threadCount = 2;
-    if (ThreadPoolNew(&tpHdl))
+    if (ThreadPoolNew(&tpHdl, THREAD_TIMEOUT_MS))
         return EXIT_FAILURE;
-    
 
+    
     ThreadPoolTask task;
     task.args = NULL;
     task.func = printHello;
     ThreadPoolTaskHandle taskHdl;
     if (launchTask(tpHdl, task, &taskHdl))
         return EXIT_FAILURE;
+    joinTask(&taskHdl);
 
+    if (launchTask(tpHdl, task, &taskHdl))
+        return EXIT_FAILURE;
     joinTask(&taskHdl);
 
     if (ThreadPoolDestroy(&tpHdl))
--- threadpool.h
+++ threadpool.h
@@ -5,17 +5,15 @@
 
 #include <stdint.h>
 #include <errno.h>
-#include <stdatomic.h>
 
-typedef  struct 
+typedef struct 
 {
     uint16_t id;
     uint16_t threadCount;
 } ThreadPoolHandle;
-
-typedef struct
+typedef struct 
 {
-    _Atomic uint8_t state;
+    uint8_t __;
 } ThreadPoolTaskHandle;
 
 typedef struct
@@ -26,13 +24,32 @@ typedef struct
 } ThreadPoolTask;
 
 
-THREAD_POOL_API errno_t ThreadPoolNew(ThreadPoolHandle*);
+/*
+Creates a new thread pool.
+@param ThreadPoolHandle* - a handle to the thread pool.
+@param uint32_t timeoutMS* - the maximum time of inactivity in MS before an worker thread will close. 
+@return errno_t - 0 upon success, non-zero value upon failure.*/
+THREAD_POOL_API errno_t ThreadPoolNew(ThreadPoolHandle*, uint32_t timeoutMS);
 
+/*
+Destroys a thread pool.
+@param ThreadPoolHandle* - a handle to the thread pool.
+@return errno_t - 0 upon success, non-zero value upon failure.*/
 THREAD_POOL_API errno_t ThreadPoolDestroy(ThreadPoolHandle*);
 
+/*
+Launches a task.
+@param ThreadPoolHandle* - a handle to the thread pool.
+@param ThreadPoolTask task - the task to launch.
+@param ThreadPoolTaskHandle* taskHdlOut - a handle to the task.
+@return errno_t - 0 upon success, non-zero value upon failure.*/
 THREAD_POOL_API errno_t launchTask(ThreadPoolHandle tpHdl, ThreadPoolTask task, ThreadPoolTaskHandle* taskHdlOut);
 
-THREAD_POOL_API errno_t joinTask(ThreadPoolTaskHandle* taskHdl);
+/*
+Waits for a task to complete 
+@param ThreadPoolTaskHandle* - the handle to wait for.
+@return errno_t - 0 upon success, non-zero value upon failure.*/
+THREAD_POOL_API void joinTask(ThreadPoolTaskHandle* taskHdl);
 
 
 #endif
